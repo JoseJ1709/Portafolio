9fc05abd6b92c7bab2b7e77180595f7ad5e46ecb
JoseJ1709 <jarammillojoseandres26@gmail.com>
2025-03-19 19:22:24 -0500

    8.0

--- assets/index.js
+++ assets/index.js
@@ -1,19 +1,17 @@
-// page navigation variables
-const navigationLinks = document.querySelectorAll("[data-nav-link]");
-const pages = document.querySelectorAll("[data-page]");
+const enlacesNavegacion = document.querySelectorAll("[data-nav-link]");
+const paginas = document.querySelectorAll("[data-page]");
 
-// add event to all nav link
-for (let i = 0; i < navigationLinks.length; i++) {
-    navigationLinks[i].addEventListener("click", function () {
-  
-      for (let i = 0; i < pages.length; i++) {
-        if (this.innerHTML.toLowerCase() === pages[i].dataset.page) {
-          pages[i].classList.add("active");
-          window.scrollTo(0, 0);
-        } else {
-          pages[i].classList.remove("active");
-        }
-      }
-  
-    });
-  }
+for (let i = 0; i < enlacesNavegacion.length; i++) {
+  enlacesNavegacion[i].addEventListener("click", function () {
+      paginas.forEach(pagina => pagina.classList.remove("activo"));
+
+      const paginaSeleccionada = this.textContent.toLowerCase().trim().replace("mí", "mi");
+
+      paginas.forEach(pagina => {
+          if (pagina.dataset.page.toLowerCase().trim() === paginaSeleccionada) {
+              pagina.classList.add("activo");
+              window.scrollTo(0, 0);
+          }
+      });
+  });
+}
